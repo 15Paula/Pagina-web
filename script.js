662d82fa90ebd92664aa4f3b13c2e6f3aa165fa2
BraulioUMNG <est.braulio.genes@unimilitar.edu.co>
2025-10-01 22:57:50 -0500

    - Se creó el archivo admin.html para la gestión de productos, con un formulario para agregar nuevos productos. - Se agregó el archivo JSON (productos.json) con datos iniciales de productos para cargar los productos existentes automaticamente tanto en catalogo como en sus respectivas categorías.

--- script.js
+++ script.js
@@ -13,3 +13,31 @@ window.addEventListener("DOMContentLoaded", () => {
   loadHTML("header-placeholder", "header.html");
   loadHTML("footer-placeholder", "footer.html");
 });
+
+async function cargarProductos(filtroCategoria = null) {
+  const res = await fetch('productos.json');
+  const productos = await res.json();
+  const contenedor = document.getElementById('catalogo-container');
+  contenedor.innerHTML = '';
+
+  productos
+    .filter(p => !filtroCategoria || p.categoria === filtroCategoria)
+    .forEach(prod => {
+      const card = document.createElement('div');
+      const esJuego = prod.categoria === 'juegos';
+
+      card.classList.add(esJuego ? 'card' : 'card2');
+      card.innerHTML = `
+        <img src="${prod.imagen}" alt="${prod.nombre}">
+        <div class="${esJuego ? 'card-content' : 'card-content2'}">
+          <div class="${esJuego ? 'inner-card' : 'inner-card2'}">
+            <h2>${prod.nombre}</h2>
+            <p>${prod.descripcion}</p>
+            <div class="${esJuego ? 'precio' : 'precio2'}">$${prod.precio}</div>
+          </div>
+        </div>
+      `;
+
+      contenedor.appendChild(card);
+    });
+}
